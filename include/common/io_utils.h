366ffdbf46f5819e24da56feb90e6a6f9f98a3e8
TristanNguyen04 <nquocdung.04@gmail.com>
2026-03-23 23:22:51 +0800

    chore: add write_s32_le to shared io utils

--- include/common/io_utils.h
+++ include/common/io_utils.h
@@ -10,5 +10,6 @@ int io_skip_bytes(FILE *fp, long count);
 int io_write_bytes(FILE *fp, const void *src, size_t size);
 int io_write_u16_le(FILE *fp, unsigned int value);
 int io_write_u32_le(FILE *fp, unsigned long value);
+int io_write_s32_le(FILE * fp, int value);
 
 #endif
