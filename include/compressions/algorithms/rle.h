8cafa8443cfc2eb4f862c3b6aeb59b83f68ca7b9
ItsMeOX <ongxuan1028@gmail.com>
2026-04-05 00:39:21 +0800

    feat: add rle uncompress algorithm

--- include/compressions/algorithms/rle.h
+++ include/compressions/algorithms/rle.h
@@ -4,13 +4,25 @@
 typedef enum {
     STATE_START,
     STATE_COUNTING
-} RLEState;
+} RLECompressionState;
 
 typedef struct {
-    RLEState state;
+    RLECompressionState state;
     char current_char;
     int count;
-} RLEMachine;
+} RLECFSM;
+
+typedef enum {
+    RLE_DSTATE_START,
+    RLE_DSTATE_READ_NUM,
+    RLE_DSTATE_WRITE
+} RLEDecompressState;
+
+typedef struct {
+    RLEDecompressState state;
+    char current_char;
+    int run_length;
+} RLEDFSM;
 
 /**
  * @brief Compress a string using Run-Length Encoding (RLE).
@@ -28,5 +40,27 @@ typedef struct {
  * - Caller must free the returned string.
  */
 char *rle_compress(const char *input);
+
+/**
+ * @brief Decompress a Run-Length Encoded (RLE) string.
+ *
+ * This function decodes a string encoded in (character, count) format
+ * back into its original form. For example:
+ *
+ *   "A4B3C2D1A2" -> "AAAABBBCCDAA"
+ *
+ * @param encoded RLE-compressed string (null-terminated)
+ *
+ * @return A newly allocated null-terminated string containing the decompressed result,
+ *         or NULL if an error occurs (e.g., input is NULL, invalid format, allocation failure).
+ *
+ * @note
+ * - The encoded string must follow the format: <char><count>, where:
+ *     - <char> is a non-digit character
+ *     - <count> is a positive integer (can be multi-digit)
+ * - Invalid formats (e.g., missing count, malformed numbers) will result in NULL.
+ * - Caller must free the returned string.
+ */
+char *rle_uncompress(const char *encoded);
 
 #endif
--- src/compressions/algorithms/rle.c
+++ src/compressions/algorithms/rle.c
@@ -3,10 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * TODO: Validate palette does not have any number
- */
-
 static void append_encoded(char *output, int *idx, char c, int count) {
     *idx += sprintf(output + *idx, "%c%d", c, count);
 }
@@ -28,7 +24,7 @@ char *rle_compress(const char *input) {
     char *output;
     int out_idx = 0;
 
-    RLEMachine fsm = {STATE_START, 0, 0};
+    RLECFSM fsm = {STATE_START, 0, 0};
 
     if (!input)
         return NULL;
@@ -67,6 +63,107 @@ char *rle_compress(const char *input) {
 
     if (fsm.state == STATE_COUNTING) {
         append_encoded(output, &out_idx, fsm.current_char, fsm.count);
+    }
+
+    output[out_idx] = '\0';
+    return output;
+}
+
+#include <stdlib.h>
+
+char *rle_uncompress(const char *encoded) {
+
+    int i, j, out_idx;
+    char c;
+    int capacity = 64;
+    char *output, *tmp;
+
+    RLEDFSM fsm = {RLE_DSTATE_START, 0};
+
+    if (!encoded)
+        return NULL;
+
+    output = malloc(capacity);
+    if (!output)
+        return NULL;
+
+    i = 0;
+    out_idx = 0;
+
+    while (encoded[i]) {
+        c = encoded[i];
+
+        switch (fsm.state) {
+
+        case RLE_DSTATE_START:
+            if ('0' <= c && c <= '9') {
+                free(output);
+                return NULL;
+            }
+
+            fsm.current_char = c;
+            fsm.run_length = 0;
+            fsm.state = RLE_DSTATE_READ_NUM;
+            i++;
+            break;
+
+        case RLE_DSTATE_READ_NUM:
+            if ('0' <= c && c <= '9') {
+                fsm.run_length = fsm.run_length * 10 + (c - '0');
+                i++;
+            } else {
+                fsm.state = RLE_DSTATE_WRITE;
+            }
+            break;
+
+        case RLE_DSTATE_WRITE:
+            if (fsm.run_length <= 0) {
+                free(output);
+                return NULL;
+            }
+
+            if (out_idx + fsm.run_length >= capacity) {
+                capacity = (out_idx + fsm.run_length) * 2;
+                tmp = realloc(output, capacity);
+                if (!tmp) {
+                    free(output);
+                    return NULL;
+                }
+                output = tmp;
+            }
+
+            for (j = 0; j < fsm.run_length; j++) {
+                output[out_idx++] = fsm.current_char;
+            }
+
+            fsm.state = RLE_DSTATE_START;
+            break;
+        }
+    }
+
+    if (fsm.state == RLE_DSTATE_READ_NUM) {
+        fsm.state = RLE_DSTATE_WRITE;
+    }
+
+    if (fsm.state == RLE_DSTATE_WRITE) {
+        if (fsm.run_length <= 0) {
+            free(output);
+            return NULL;
+        }
+
+        if (out_idx + fsm.run_length >= capacity) {
+            capacity = (out_idx + fsm.run_length) * 2;
+            tmp = realloc(output, capacity);
+            if (!tmp) {
+                free(output);
+                return NULL;
+            }
+            output = tmp;
+        }
+
+        for (j = 0; j < fsm.run_length; j++) {
+            output[out_idx++] = fsm.current_char;
+        }
     }
 
     output[out_idx] = '\0';
