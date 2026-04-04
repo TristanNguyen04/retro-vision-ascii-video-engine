fa49cfc0a2e814b5a27e55a131cf4fc7b52f0ce1
ItsMeOX <ongxuan1028@gmail.com>
2026-04-04 15:59:45 +0800

    doc: add doc for rle_compress function

--- include/compressions/algorithms/rle.h
+++ include/compressions/algorithms/rle.h
@@ -12,6 +12,18 @@ typedef struct {
     int count;
 } RLEMachine;
 
+/**
+ * @brief Compress a string using Run-Length Encoding (RLE).
+ *
+ * This function encodes consecutive repeated characters in the input string
+ * into a compact form of (character, count). For example:
+ *
+ *   "AAAABBBCCDAA" -> "A4B3C2D1A2"
+ *
+ * @return A newly allocated null-terminated string containing the RLE-compressed result.
+ *         The caller is responsible for freeing the returned string using free().
+ *
+ */
 char *rle_compress(const char *input);
 
 #endif
